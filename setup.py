bb4127c0fa3439d7bd4f6dd525ac3547eac9e685
Grigory Bakunov <thebobuk@ya.ru>
2016-11-29 14:57:10 +0300

    setuptools

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(name="urpc", version="0.0.4",
+      py_modules=['urpc'],
+      url="http://github.com/bobuk/urpc",
+      author="Grigory Bakunov",
+      author_email='thebobuk@ya.ru',
+      description='uRPC is oversimplistic RPC over Redis',
+      install_requires=[
+          'redis'
+      ],
+      classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+
+)
