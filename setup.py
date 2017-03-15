3109d84045a4eff0b931ab7e2f7e53ee66b5af48
Grigory Bakunov <thebobuk@ya.ru>
2017-03-15 23:47:13 +0300

    version bump

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-setup(name="urpc", version="0.1.0",
+setup(name="urpc", version="0.1.1",
       py_modules=['urpc'],
       url="http://github.com/bobuk/urpc",
       author="Grigory Bakunov",
